3aef31f00eb320255f82c56fad144a00ebb9effa
Amin <aminntfnd@gmail.com>
2024-03-31 14:45:46 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Celestine
+Celestine is my experimental programming language. I first started with the idea of improving C but it's getting a bit far away from just that.
+
+As of now, this is pretty much the only program supported and only on 64-bit linux:
+```ts
+// prints "Hi\n"
+function main(): int = {
+    putchar 72;  // H
+    putchar 105; // i
+    putchar 10;  // \n
+    return 0;
+}
+```
+
+## Dependencies
+- [QBE](https://c9x.me/compile/)
+- Python (tested with 3.11)
+- A compiler for GAS style assembly (tested with GCC)
+
+## Usage
+```console
+usage: main.py [-h] [-r] [-p] [-k] file
+
+Celestine compiler.
+
+positional arguments:
+  file
+
+options:
+  -h, --help   show this help message and exit
+  -r, --run    run executable output
+  -p, --print  print generated AST
+  -k, --keep   don't delete file from each compiling stage
+```
+
+## Design
+Celestine tries to be a low level system programming language that has support for a subset of modern language features but doesn't stray much far from C.
+
+A Celestine program consists of a main function which itself is made os statements. Every statement ends with a semicolon(`;`). There are two kinds of statements as of right now:
+1. putchar which prints given a number prints it's corresponding ASCII character. It exists as a way to test the language and will be removed or replaced ved with a function in the future.
+2. return which ends the function returning the given number.
+
+### Comments
+Single lined comments are denoted by usage of `//`.
+As of right now, there's no support other types of comments.
+
+### Expressions
+Statements accept expressions and as of right now only 64-bit integers are supported and every expression evaluates to an integer.
+
+#### Arithmetic operations
+You can do these arithmetic operations in an expression using an infix notation:
+|operator|operation       |
+|--------|----------------|
+|`+`     | Addition       |
+|`-`     | Subtraction    |
+|`*`     | Multiplication |
+|`/`     | Division       |
+
+Also you can group operations with parenthesis. Order of operations is the same as in math.
+
+Example:
+```ts
+// prints 1 as 1+48 is the ASCII code for '1'
+putchar ((2 + 1) * 2) / 3 - 1 + 48;
+```
+
---
+++ main.py
@@ -0,0 +1,518 @@
+#!/usr/bin/python3
+
+from typing import TextIO, Optional, Tuple
+from abc import abstractmethod, ABCMeta
+from collections.abc import Iterator
+
+from enum import Enum, auto
+from dataclasses import dataclass
+
+from sys import stderr
+from os import PathLike
+from pathlib import Path
+from subprocess import run
+from argparse import ArgumentParser, Namespace
+
+
+class UniqueId:
+    def __init__(self, prefix: str) -> None:
+        self.counter = -1
+        self.prefix = prefix
+
+    def __call__(self) -> str:
+        self.counter += 1
+        return f"{self.prefix}{self.counter}"
+
+
+VAR_NAME_GEN = UniqueId("v")
+DEBUG = True
+
+
+class TokenKind(Enum):
+    # Single-character tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    SEMICOLON = auto()
+    COLON = auto()
+    ASSIGNMENT = auto()
+    PLUS = auto()
+    MINUS = auto()
+    MULTIPLY = auto()
+    DIVIDE = auto()
+    # One or two character tokens
+    # Literals
+    IDENTIFIER = auto()
+    INTEGER = auto()
+    # Keywords
+    PUTCHAR = auto()  # TODO : remove in future
+    FUNCTION = auto()
+    INT = auto()
+    RETURN = auto()
+
+
+@dataclass
+class Token:
+    kind: TokenKind
+    offset: int
+    lexeme: str
+
+
+class CompilerError(Exception):
+    def __init__(
+        self,
+        msg: str,
+        text: TextIO,
+        offset: int,
+        *args: object,
+        filepath: Optional[PathLike] = None,
+    ) -> None:
+        (line, col) = CompilerError.get_location(text, offset)
+        location = f"{filepath}:{line}:{col}" if filepath else f"code:{line}:{col}"
+        super().__init__(f"{location} : error: {msg}", *args)
+
+    @staticmethod
+    def get_location(
+        text: TextIO,
+        offset: int,
+    ):
+        text.seek(0)
+
+        content = text.read(offset)
+        if not content:
+            return (1, 1)
+
+        sp = content.splitlines(keepends=True)
+        return (len(sp), len(sp[-1]) + 1)  # We need column value counted from one
+
+
+class EndOfTokens(CompilerError, StopIteration):
+    def __init__(
+        self, text: TextIO, offset: int, *args: object, filepath: PathLike | None = None
+    ):
+        super().__init__(
+            "Unexpected end of tokens", text, offset, *args, filepath=filepath
+        )
+
+
+class Lexer(Iterator):
+    single_char_tokens = {
+        "(": TokenKind.LEFT_PAREN,
+        ")": TokenKind.RIGHT_PAREN,
+        "{": TokenKind.LEFT_BRACE,
+        "}": TokenKind.RIGHT_BRACE,
+        ";": TokenKind.SEMICOLON,
+        ":": TokenKind.COLON,
+        "=": TokenKind.ASSIGNMENT,
+        "+": TokenKind.PLUS,
+        "-": TokenKind.MINUS,
+        "*": TokenKind.MULTIPLY,
+        "/": TokenKind.DIVIDE,
+    }
+
+    keywords = {
+        "putchar": TokenKind.PUTCHAR,
+        "function": TokenKind.FUNCTION,
+        "int": TokenKind.INT,
+        "return": TokenKind.RETURN,
+    }
+
+    def __init__(self, text: TextIO, path: Optional[PathLike] = None):
+        self._position = 0
+        self._text = text
+        self._peeked = False
+        self._peeked_token = None
+        self.path = path
+        assert self._text.seekable()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Token:
+        if self._peeked:
+            self._peeked = False
+            return self._peeked_token
+
+        c = self._forward_char()
+
+        while c.isspace():
+            c = self._forward_char()
+
+        if not c:
+            self._position -= 1
+            raise EndOfTokens(self.text, self._position, filepath=self.path)
+
+        kind = self.single_char_tokens.get(c)
+
+        # handle two or more character tokens
+        if kind == TokenKind.DIVIDE and self._peek_char() == "/":
+            # A line comment
+            while c and c != "\n":
+                c = self._forward_char()
+            return self.next()
+
+        if not kind:
+            self._back_char()
+            if c.isdigit():
+                return self.parse_int()
+            elif c.isalpha() or c == "_":
+                return self.parse_id()
+            else:
+                raise CompilerError(
+                    f"Unrecognized token '{c}'.", self._text, self._position, self.path
+                )
+
+        return Token(kind, self._position, c)
+
+    def __del__(self):
+        self._text.close()
+
+    @property
+    def text(self):
+        return self._text
+
+    def _back_char(self):
+        """Move back one char"""
+        self._position -= 1
+        self._text.seek(self._position)
+
+    def _forward_char(self) -> str:
+        """Move one char forward"""
+        self._text.seek(self._position)
+        self._position += 1
+        return self._text.read(1)
+
+    def _peek_char(self) -> str:
+        """Look at next char without moving"""
+        token = self._forward_char()
+        self._back_char()
+        return token
+
+    def next(self):
+        """Get next token"""
+        return next(self)
+
+    def peek(self):
+        """Look at next token without moving"""
+        self._peeked_token = self.next()
+        self._peeked = True
+        return self._peeked_token
+
+    def parse_int(self) -> Token:
+        pos = self._position
+        s = []
+        while (c := self._forward_char()).isdigit():
+            s.append(c)
+        self._back_char()
+        s = "".join(s)
+
+        return Token(TokenKind.INTEGER, pos, s)
+
+    def parse_id(self) -> Token:
+        pos = self._position
+        s = []
+        c = self._forward_char()
+        while c.isalnum() or c == "_":
+            s.append(c)
+            c = self._forward_char()
+        self._back_char()
+
+        s = "".join(s)
+        kind = self.keywords.get(s) or TokenKind.IDENTIFIER
+
+        return Token(kind, pos, s)
+
+    @staticmethod
+    def lex_file(path: PathLike):
+        fp = open(path, encoding="UTF-8")
+        return Lexer(fp, path)
+
+    def expect_token(self, kind: TokenKind) -> Token:
+        token = self.next()
+        if token.kind != kind:
+            raise CompilerError(
+                f"Expected '{kind}' but got {token}",
+                self._text,
+                self._position,
+                filepath=self.path,
+            )
+        return token
+
+
+class AST:
+    __metaclass__ = ABCMeta  # pylint only works correctly like this
+
+    @abstractmethod
+    def __repr__(self) -> str: ...
+
+    @abstractmethod
+    def to_ir(self) -> tuple[Optional[str], str]:
+        """Returns a tuple of possible variable name
+        for result of expression and the IR"""
+
+
+class ASTParser(AST):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, lexer: Lexer): ...
+
+
+class IntLiteral(AST):
+    def __init__(self, lexeme: str) -> None:
+        assert lexeme.isdigit()
+        self.lexeme = lexeme
+
+    def to_ir(self):
+        var_name = VAR_NAME_GEN()
+        return (var_name, f"\n    %{var_name} =l copy {self.lexeme}")
+
+    def __repr__(self) -> str:
+        return f"IntLiteral({self.lexeme})"
+
+
+class BinaryOp(AST):
+    arithmetic_instructions = {
+        TokenKind.PLUS: "add",
+        TokenKind.MINUS: "sub",
+        TokenKind.MULTIPLY: "mul",
+        TokenKind.DIVIDE: "div",
+    }
+
+    def __init__(
+        self,
+        op: TokenKind,
+        left: "Expr",
+        right: "Expr",
+    ):
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"BinaryOp(op={self.op}, left={self.left}, right={self.right})"
+
+    def to_ir(self) -> Tuple[str | None]:
+        (left_var, left_ir) = self.left.to_ir()
+        (right_var, right_ir) = self.right.to_ir()
+        self_var = VAR_NAME_GEN()
+        instruction = self.arithmetic_instructions.get(self.op)
+        self_ir = f"\n    %{self_var} =l {instruction} %{left_var}, %{right_var}"
+        return (self_var, left_ir + right_ir + self_ir)
+
+
+class Expr(ASTParser):
+    def __init__(self, lexer: Lexer):
+        self.lexer = lexer
+        self.child = self.parse()
+
+    def __repr__(self) -> str:
+        return str(self.child)
+
+    def to_ir(self):
+        return self.child.to_ir()
+
+    def parse(self, precedence=0):
+        """An implementation of a Pratt parser"""
+
+        token = self.lexer.peek()
+
+        prefix = self.prefix_parse_functions.get(token.kind)
+        if not prefix:
+            raise CompilerError(
+                f"Unexpected token '{token}' while parsing expression.",
+                self.lexer.text,
+                token.offset,
+                filepath=self.lexer.path,
+            )
+
+        left = prefix(self)
+        token = self.lexer.peek()
+
+        while precedence < self.precedence(token):
+            left = self.parse_infix(token.kind, left)
+            token = self.lexer.peek()
+
+        return left
+
+    def precedence(self, token):
+        return self.precedences.get(token.kind, 0)
+
+    def parse_integer(self):
+        token = self.lexer.next()
+        return IntLiteral(token.lexeme)
+
+    def parse_group(self):
+        self.lexer.next()
+        result = self.parse()
+        self.lexer.expect_token(TokenKind.RIGHT_PAREN)
+        return result
+
+    def parse_infix(self, op: TokenKind, left):
+        self.lexer.next()
+        right = self.parse(self.precedences[op])
+        return BinaryOp(op, left, right)
+
+    precedences = {
+        TokenKind.PLUS: 1,
+        TokenKind.MINUS: 1,
+        TokenKind.MULTIPLY: 2,
+        TokenKind.DIVIDE: 2,
+    }
+
+    prefix_parse_functions = {
+        TokenKind.INTEGER: parse_integer,
+        TokenKind.LEFT_PAREN: parse_group,
+    }
+
+
+class Statement(ASTParser):
+    def __init__(self, lexer: Lexer):
+        token = lexer.next()
+        if token.kind not in (TokenKind.RETURN, TokenKind.PUTCHAR):
+            raise CompilerError(
+                f"Unexpected token '{token}'.",
+                lexer._text,
+                lexer._position,
+                filepath=lexer.path,
+            )
+        self.kind = token.kind
+        self.expr = Expr(lexer)
+        lexer.expect_token(TokenKind.SEMICOLON)
+
+    def __repr__(self) -> str:
+        return f"{self.kind.name}({self.expr}),"
+
+    def to_ir(self):
+        (var_name, res) = self.expr.to_ir()
+        match self.kind:
+            case TokenKind.PUTCHAR:
+                res += f"\n    call $putchar(l %{var_name})"
+            case TokenKind.RETURN:
+                res += f"\n    ret %{var_name}"
+            case _:
+                raise TypeError(f"Can't use type {self.kind} as statement type")
+        return (None, res)
+
+
+class Function(ASTParser):
+    name: str
+    body: list[Statement]
+
+    def __init__(self, lexer: Lexer):
+        # For now we only parse a main function that has no arguments
+        lexer.expect_token(TokenKind.FUNCTION)
+        self.name = lexer.expect_token(TokenKind.IDENTIFIER).lexeme
+        assert self.name == "main"
+        lexer.expect_token(TokenKind.LEFT_PAREN)
+        lexer.expect_token(TokenKind.RIGHT_PAREN)
+        lexer.expect_token(TokenKind.COLON)
+        lexer.expect_token(TokenKind.INT)
+        lexer.expect_token(TokenKind.ASSIGNMENT)
+        lexer.expect_token(TokenKind.LEFT_BRACE)
+
+        self.body = []
+        while True:
+            self.body.append(Statement(lexer))
+            if self.body[-1].kind == TokenKind.RETURN:
+                break
+
+        lexer.expect_token(TokenKind.RIGHT_BRACE)
+
+    def __repr__(self) -> str:
+        body = "\n    " + "\n    ".join(map(str, self.body))
+        return f"Function(name={self.name}, body={body}\n  )\n"
+
+    def to_ir(self) -> tuple[None, str]:
+        top = f"\nexport function l ${self.name}() {{\n@start"
+        bottom = "\n}\n"
+        body = "".join(map(lambda x: x.to_ir()[1], self.body))
+        return (None, top + body + bottom)
+
+
+class Program(ASTParser):
+    main: Function
+
+    def __init__(self, lexer: Lexer):
+        self.main = Function(lexer)
+
+    def __repr__(self) -> str:
+        return f"Program({self.main})"
+
+    def to_ir(self) -> tuple[None, str]:
+        return (
+            None,
+            """
+function w $pushchar(l %c) { \n\
+@start 
+    %a =l alloc4 8 \n\
+    storew %c, %a \n\
+    %b =w call $write(w 1, l %a, w 1) \n\
+    ret %b \n\
+} \n"""
+            + self.main.to_ir()[1],
+        )
+
+
+def compile_file(args: Namespace) -> Path:
+    path = Path(args.file)
+    ssa_file = path.parent.joinpath(f"{path.stem}.ssa")
+    asm_file = path.parent.joinpath(f"{path.stem}.s")
+    out_file = path.parent.joinpath(f"{path.stem}.out")
+
+    lexer = Lexer.lex_file(path)
+    program = Program(lexer)
+
+    if args.print:
+        print(program, file=stderr)
+
+    with open(ssa_file, "w", encoding="UTF-8") as f:
+        f.write(program.to_ir()[1])
+
+    run(
+        ["qbe", "-o", asm_file, ssa_file],
+        check=True,
+    )
+
+    run(["cc", asm_file, "-o", out_file], check=True)
+
+    if not args.keep:
+        ssa_file.unlink()
+        asm_file.unlink()
+
+    return out_file
+
+
+def main():
+    arg_parser = ArgumentParser(description="Celestine compiler.")
+    arg_parser.add_argument(
+        "-r", "--run", help="run executable output", action="store_true"
+    )
+    arg_parser.add_argument(
+        "-p", "--print", help="print generated AST", action="store_true"
+    )
+    arg_parser.add_argument(
+        "-k",
+        "--keep",
+        help="don't delete files from each compiling stage",
+        action="store_true",
+    )
+    arg_parser.add_argument("file")
+
+    args = arg_parser.parse_args()
+    try:
+        out_path = compile_file(args)
+
+    except CompilerError as ce:
+        if DEBUG:
+            raise ce
+        print(ce)
+        exit(-1)
+
+    if args.run:
+        run([out_path.absolute()], check=True)
+
+
+if __name__ == "__main__":
+    main()
